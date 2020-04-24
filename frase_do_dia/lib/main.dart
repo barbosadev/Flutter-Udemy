7f2d2f7e630b8869401d92908cf69e1e640c5caf
Victor Barbosa <vicldb99@gmail.com>
2020-04-24 15:43:52 -0300

    Espaçamentos

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Frases do dia",
       home: Container(
-        color: Colors.white,
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.blue)
+        ),
+        padding: EdgeInsets.fromLTRB(10, 60, 10, 0),
+        margin: EdgeInsets.all(30),
+        //color: Colors.white,
         child: Column(
           children: <Widget>[
             Text(
               "Lorem Ipsum",
               style: TextStyle(
                 fontSize: 40,
-                color: Colors.black45,
+                color: Colors.white,
                 fontWeight: FontWeight.w800,
                 letterSpacing: 10,
                 decoration: TextDecoration.none
@@ -27,12 +33,17 @@ void main() {
                   "Botão",
                 style: TextStyle(
                   fontSize: 40,
-                  color: Colors.black45,
+                  color: Colors.white,
                   decoration: TextDecoration.none
                 ),
               ),
-              
             ),
+            Text("t1"),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text("t2"),
+            ),
+            Text("t3"),
           ],
         ),
       )
