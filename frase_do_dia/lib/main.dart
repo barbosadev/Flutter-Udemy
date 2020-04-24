2163881e9a7ddd9aec38b8989fd6f9205ed0983c
Victor Barbosa <vicldb99@gmail.com>
2020-04-23 23:56:16 -0300

    Column e Row

---
+++ frase_do_dia/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Frases do dia",
+      home: Row(
+        children: <Widget>[
+          Text('T1: '),
+          Text('T2: '),
+        ],
+      ),
+  ));
+}
