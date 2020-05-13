1736aa6ff76ad0a39e40b95e992782d86ac37136
Victor Barbosa <vicldb99@gmail.com>
2020-05-12 23:22:28 -0300

    Update Home.dart

--- consumo_servico_avancado/lib/Home.dart
+++ consumo_servico_avancado/lib/Home.dart
@@ -25,6 +25,40 @@ class _HomeState extends State<Home> {
     return postagens;
   }
 
+  _post() async{
+
+    var corpo = json.encode(
+        {
+          "userId" : 1,
+          "id": null,
+          "title" : "Novo Post",
+          "body" : "Esse é o novo post!!!"
+        }
+    );
+
+
+    http.Response response = await http.post(
+      _urlBase + "/posts",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: corpo
+    );
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  _put(){
+
+  }
+  _patch(){
+
+  }
+
+  _delete(){
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,40 +66,65 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Consumo de Serviço Avançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        //initialData: ,
-        future: _recuperarPostagens(),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if(snapshot.hasError){
-                print("Lista: Erro ao Carregar");
-              } else {
-                print("Lista Carregada");
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index){
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Salvar"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Salvar"),
+                  onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Salvar"),
+                  onPressed: _post,
+                ),
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                //initialData: ,
+                  future: _recuperarPostagens(),
+                  builder: (context, snapshot){
+                    switch(snapshot.connectionState){
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                        break;
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        if(snapshot.hasError){
+                          print("Lista: Erro ao Carregar");
+                        } else {
+                          print("Lista Carregada");
+                          return ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, index){
 
-                    List<Post> lista = snapshot.data;
-                    Post post = lista[index];
+                                List<Post> lista = snapshot.data;
+                                Post post = lista[index];
 
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.id.toString()),
-                    );
+                                return ListTile(
+                                  title: Text(post.title),
+                                  subtitle: Text(post.id.toString()),
+                                );
+                              }
+                          );
+                        }
+                        break;
+                    }
                   }
-                );
-              }
-              break;
-          }
-        }
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
